4ea0e4cbc3970962e6363cb73e725157ea89598c
NkRyan1 <nkryanlx@gmail.com>
2022-04-14 17:44:12 -0700

    Just added a directory

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -10,44 +10,34 @@
 
 int getMaxPrimeFactor(long int n) 
 {
+	int i, max;
 
-	      int i, max;
-		  
-	      max = -1;
-
-	      while(n % 2 == 0) 
-	      {
-
-	            max = 2;
-		    n = n / 2;
-
-	      }
-
-	      for(i = 3; i <= sqrt(n); i=i+2)
-	      {
-		      while(n % i == 0) 
-		      {
-		              max = i;
-			      n = n / i;
-		      }
-	      }
-              if(n > 2)
-	      {
-		      max = n;
- 	      }
-	      return max;
-
+	max = -1;
+       	while(n % 2 == 0) 
+	{
+		max = 2;
+		n = n / 2;
+	}
+	for(i = 3; i <= sqrt(n); i=i+2)
+	{
+	       	while(n % i == 0)
+		{
+		       	max = i;
+			n = n / i;
+		}
+	}
+	if(n > 2)
+	{
+		max = n;
+	}
+	return max;
 }
 
 int main()
 {
-
 	long int n;
 
 	n = getMaxPrimeFactor(612852475143);
-
 	printf("%ld\n", n);
-
 	return (0);
-
 }
