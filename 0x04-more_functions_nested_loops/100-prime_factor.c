1190e0d40b1401044768c4870ac10bd0f3e11e1e
NkRyan1 <nkryanlx@gmail.com>
2022-04-15 15:13:32 -0700

    Just added a new file

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -11,33 +11,32 @@ int getMaxPrimeFactor(long int n)
 int main()
 {
 	long int n;
-
+	
 	n = getMaxPrimeFactor(612852475143);
 	printf("%ld\n", n);
 	return (0);
 }
 
 int getMaxPrimeFactor(long int n)                                                                
-{                                                                                                
-        int i, max;                                                                              
-                                                                                                
-        max = -1;                                                                                
-        while(n % 2 == 0)                                                                        
-	{                                                                                        
-		max = 2;                                                                         
-	        n = n / 2;                                                                       
-        }                                                                                        
-        for(i = 3; i <= sqrt(n); i=i+2)                                                          
-        {                                                                                        
-                while(n % i == 0)                                                                
-		{                                                                                
-			max = i;                                                                 
-			n = n / i;                                                               
-                }                                                                                
-        }                                                                                        
-        if(n > 2)                                                                                
-        {                                                                                        
-	        max = n;                                                                         
+{
+	int i, max;
+	max = -1;
+	while(n % 2 == 0)
+	{
+		max = 2;
+		n = n / 2;                                                                       
+        }
+	for(i = 3, i <= sqrt(n); i=i+2)
+	{
+		while(n % i == 0)
+		{
+			max = i;
+			n = n / i;
+		}
 	}
-	return max;                                                                              
-} 
+	if(n > 2)
+	{
+		max = n;
+	}
+	return max;
+}
