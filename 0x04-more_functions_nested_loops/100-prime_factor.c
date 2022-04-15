bdd6e95326e651c452abd64a674eb72c48e92049
NkRyan1 <nkryanlx@gmail.com>
2022-04-14 17:21:51 -0700

    Just added a directory

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,24 +1,51 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<math.h>
+
+/**
+ *
+ * main - finding the greatest prime factor of a number
+ *
+ * Return: 0
+ */
+
+int getMaxPrimeFactor(int n) 
+{
+
+	      int i, max = -1;
+
+	      while(n % 2 == 0) 
+	      {
+
+	            max = 2;
+		    n = n / 2; //reduce n by dividing this by 2
+
+	      }
+
+	      for(i = 3; i <= sqrt(n); i=i+2)
+	      {
+		      while(n % i == 0) 
+		      {
+		              max = i;
+			      n = n / i;
+		      }
+	      }
+              if(n > 2)
+	      {
+		      max = n;
+ 	      }
+	      return max;
+
+}
 
 int main()
 {
-	long int n;
-	n=3453;
-	long int div=2, ans = 0, maxFact;
-	while(n!=0)
-	{
-		if(n % div !=0)	
-			div = div + 1;
-		else
-		{
-			maxFact = n;
-			n = n / div;
-			if(n == 1)
-			{
-				printf("%d is the largest prime factor !",maxFact);
-				ans = 1;
-				break;
-			}
-		}
-	}
+
+	int n;
+
+	n = getMaxPrimeFactor(612852475143);
+
+	printf("Max prime factor: %d", n);
+
+	return (0);
+
 }
