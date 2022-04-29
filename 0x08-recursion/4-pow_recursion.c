9425c6354f4b1cbf5b94d1337376b042571e5fe1
NkRyan1 <nkryanlx@gmail.com>
2022-04-28 18:52:37 -0700

    just added a file

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * _pow_recursion - Search a string for any of a set of bytes.
@@ -11,13 +11,13 @@
 int _pow_recursion(int x, int y)
 {
 
-if (y < 0)
-	return (-1);
-else if (y == 1)
-	return (x);
-else if (y == 0)
-	return (1);
+	if (y < 0)
+		return (-1);
+	else if (y == 1)
+		return (x);
+	else if (y == 0)
+		return (1);
 
-return (x * _pow_recursion(x, y - 1));
+	return (x * _pow_recursion(x, y - 1));
 
 }
