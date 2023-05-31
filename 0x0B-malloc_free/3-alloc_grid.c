91aa7a8394c71fdae4ae6f6259299d52eeb6a6cd
NkRyan1 <101342649+NkRyan1@users.noreply.github.com>
2023-05-31 19:24:12 +0100

    Create 3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *alloc_grid - main block
+ *Description: Creates a 2 dimensional array.
+ *@r: Number of rows.
+ *@c: Number of columns.
+ * Return: a pointer or NULL when it fails
+ *
+ */
+
+char *alloc_grid(int c, int r)
+{
+	int **s;
+	int i;
+	int j;
+	
+	s = (int **)malloc(sizeof(int) * r);
+        if (s == NULL)
+                return (NULL);
+        for (i = 0; i < r; i++)
+        {
+		s[i] = (int *)malloc(sizeof(int)*c);
+		if (s[i] == NULL)
+			return (NULL);
+		for (j = 0; j < c; j++)
+			s[i][j] = 0;
+	}
+	return (s);
+}
+
