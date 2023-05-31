3101da715d0924f8af0b8ec6a9ce2afe10c856fa
NkRyan1 <101342649+NkRyan1@users.noreply.github.com>
2023-05-31 17:26:43 +0100

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,38 +18,75 @@ char *str_concat(char *str1, char *str2)
 	int lg1;
 	int i;
 
-	if ((str1 == NULL)||(str2 == NULL))
+	if ((str1 == NULL) || (str2 == NULL))
 	{
+		if (str1 == NULL)
+		{
+			lg1 = 1 + strlen(str2);
+			s = (char *)malloc(sizeof(char) * lg1);
+			if (s == NULL)
+			{
+				return (NULL);
+			}
+			while (i <= lg1 - 1)
+			{
+				if (i == lg1 - 1)
+				{
+					s[i] = '\0';
+				}
+				else
+				{
+					s[i] = str2[i];
+				}
+				i++;
+			}
+			return (s);
+		}
+		else if (str2 == NULL)
+		{
+			lg1 = 1 + strlen(str1);
+			s = (char *)malloc(sizeof(char) * lg1);
+			if (s == NULL)
+			{
+				return (NULL);
+			}
+			while (i <= lg1 - 1)
+			{
+				if (i == lg1 - 1)
+				{
+					s[i] = '\0';
+				}
+				else
+				{
+					s[i] = str1[i];
+				}
+				i++;
+			}
+			return (s);
+		}
 		return (NULL);
 	}
 	lg = 1 + strlen(str1) + strlen(str2);
 	lg1 = 1 + strlen(str1);
-	if (lg == 1)
-	{
-		s = (char *)malloc(sizeof(char) * lg);
-		if (s == NULL)
-		{
-			return (NULL);
-		}
-		s[0] = '\0';
-		return (s);
-	}
 	s = (char *)malloc(sizeof(char) * lg);
 	if (s == NULL)
 	{
 		return (NULL);
 	}
 	i = 0;
-	while (i < lg1 - 1)
+	while (i <= lg1 - 1)
 	{
-		s[i] = str1[i];
+		if (i == lg1 - 1)
+		{
+			s[i] = '\0';
+		}
+		else
+		{
+			s[i] = str1[i];
+		}
 		i++;
 	}
 	strcat(s, str2);
-/**
- * 	s[lg - 1] = '\0';
- *
- */ 
 	return (s);
 	free(s);
 }
